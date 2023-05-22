2727aa5f0ebbc360bc8f91d67f1063b181c3fd89
manon-coulombe <manon.coulombe@student.adatechschool.fr>
2023-05-22 23:39:55 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        pinkBellacoso: '#e969a8'
+      },
+    },
+  },
+  plugins: [],
+}
+
